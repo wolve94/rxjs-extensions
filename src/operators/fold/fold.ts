3fa32840331a07d8630c06fe1588cc4b539b3759
wolfi.r.94 <wolfi.r.94@gmail.com>
2022-03-16 07:24:17 +0100

    jsdoc for fold created

--- src/operators/fold/fold.ts
+++ src/operators/fold/fold.ts
@@ -1,6 +1,12 @@
 import { Observable, OperatorFunction } from 'rxjs';
 import { reduce } from 'rxjs/operators';
 
+/**
+ * creates an array out of all values of the source observable as soon as it has completed
+ *
+ * @example of(1, 2, 3, 4).pipe(fold()).subscribe(console.log) -> would print the array [1, 2, 3, 4]
+ * => Empty observable are converted into empty arrays
+ */
 export function fold<T>(): OperatorFunction<T, T[]> {
   return (source: Observable<T>): Observable<T[]> => {
     return source.pipe(
